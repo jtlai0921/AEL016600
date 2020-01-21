d1b0c352ca0165c01cff7faec8b6f1d4209f32f6
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-01-21 16:22:01 +0800

    Initial commit

---
+++ Samples/Ch17/hello.php
@@ -0,0 +1,13 @@
+﻿<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>我的第一個PHP程式</title>
+  </head>
+  <body>
+    <?php  
+      echo("Hello World!");
+      phpinfo();
+    ?>
+  </body>
+</html>
